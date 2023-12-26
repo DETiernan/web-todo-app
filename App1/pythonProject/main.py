e6576d215ef22754207dc6f6afc83635984da684
david <david.tiernan@outlook.com>
2023-12-26 15:18:37 +1000

    formatting

--- App1/pythonProject/main.py
+++ App1/pythonProject/main.py
@@ -1,10 +1,18 @@
 # This program creates a todolist in a file
 import time
 
-def get_todos(filepath):
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of
+    do-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath="todos.txt")
+    """ Write the to-dos items list in the text file. """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
 
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
