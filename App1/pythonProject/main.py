bfe0e913365b11d0ad8688786b3bc1bc274957a0
david <david.tiernan@outlook.com>
2023-12-26 15:19:24 +1000

    formatting

--- App1/pythonProject/main.py
+++ App1/pythonProject/main.py
@@ -9,7 +9,7 @@ def get_todos(filepath="todos.txt"):
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt")
+def write_todos(todos_arg, filepath="todos.txt"):
     """ Write the to-dos items list in the text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
